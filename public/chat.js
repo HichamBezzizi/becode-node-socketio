80f9be3d7c3d5060cd7130584bc59b1efa392c61
Hicham Bezzizi <hicham_bezzizi@hotmail.com>
2019-09-10 21:04:02 +0200

    .

--- public/chat.js
+++ public/chat.js
@@ -1,38 +1,39 @@
 // Socket connection
-var frontendSocket = io.connect("http://localhost:1234");
+let frontendSocket = io.connect("http://localhost:1234");
 
 
 //DOM setup
-var chatMessage = document.getElementById("chatMessage");
-var userName = document.getElementById("userName");
-var chatButton = document.getElementById("chatButton");
-var chatData = document.getElementById("chatData");
-var users = document.getElementById("users");
-var feedback = document.getElementById("feedBack");
+let chatMessage = document.getElementById("chatMessage");
+let userName = document.getElementById("userName");
+let chatButton = document.getElementById("chatButton");
+let chatData = document.getElementById("chatData");
+let users = document.getElementById("users");
+let feedback = document.getElementById("feedBack");
+let connectUsers = document.getElementById("onlineusers");
 
 
 //random colors for the chat usernames
-var colors = [
-     "#df0bbe", 
-     "#e0a316",
-     "#64bd52",
-     "#f1075a",
-     "#64bd52",
-     "#d28c47",
+let colors = [
+    "#df0bbe",
+    "#e0a316",
+    "#64bd52",
+    "#f1075a",
+    "#64bd52",
+    "#d28c47",
 ];
-var random_color = colors[Math.floor(Math.random() * colors.length)];
+let random_color = colors[Math.floor(Math.random() * colors.length)];
 document.getElementById("chatData").style.color = random_color;
 
 //Eventlistener for enter key when submitting a msg
-chatMessage.addEventListener("keydown", function(e) {
+chatMessage.addEventListener("keydown", function (e) {
     if (e.keyCode === 13) {
-      e.preventDefault();
-      document.getElementById("chatButton").click();
+        e.preventDefault();
+        document.getElementById("chatButton").click();
     }
-  });
+});
 
 //Event for emit
-chatButton.addEventListener("click",function(){
+chatButton.addEventListener("click", function () {
     frontendSocket.emit("chat", {
         chatmessage: chatMessage.value,
         username: userName.value,
@@ -42,42 +43,45 @@ chatButton.addEventListener("click",function(){
 
 
 //Event for chat
-chatMessage.addEventListener("keypress", function(){
+chatMessage.addEventListener("keypress", function (e) {
     frontendSocket.emit("typing", userName.value)
     if (e.keyCode === 13) {
         e.preventDefault();
         document.getElementById("chatButton").click();
-      }
+    }
+
 });
 
 
 //Event for listen
-frontendSocket.on("chat", function(data){
+frontendSocket.on("chat", function (data) {
     feedback.innerHTML = ""
     chatData.innerHTML += "<p><strong>" + data.username + ": </strong>" + data.chatmessage + "</p>"
     //makes the chat scroll
-    document.getElementById("chatData").lastChild.scrollIntoView({block: "end", behavior: "smooth"})
+    document.getElementById("chatData").lastChild.scrollIntoView({ block: "end", behavior: "smooth" })
 });
 
-frontendSocket.on("typing", function(data){
+frontendSocket.on("typing", function (data) {
     feedback.innerHTML = "<p><em>" + data + " is typing a message...</em></p>"
 });
 
 
 
 //Event for users
-frontendSocket.on("get users", function(data){
+frontendSocket.on("get users", function (data) {
     //this will display the users in the frontend
     users.textContent = "Users connected: " + data;
 });
 
 
-//Event for logged in users
-frontendSocket.on("get a user",(data)=>{
-    let html =""
-    for(i=0; i < data.length; i++){
-        //html += '<li class="list-group-item>"${data[!]}</li>';
+//Event for logged in users (WIP)
+frontendSocket.on("get a user", (data) => {
+    let html = ""
+    for (i = 0; i < data.length; i++) {
+        // html += `<li class="list-group-item>"${data[i]}</li>`;
+        html += `<div id = "onlineUsers">Current users online:${data[i]} </div>`
     }
-    users.html(html);
+    connectUsers.innerHTML = html;
+    console.log(data)
 });
 
